129b7901770f74efa4e287b3d6ef722b20f9df2e
Lucas-SousaS <lucassousa4019@gmail.com>
2024-08-18 21:30:21 -0300

    feat: areaTriangulo

--- 3 Bimestre/Exercícios/areaTriangulo.php
+++ 3 Bimestre/Exercícios/areaTriangulo.php
@@ -1,10 +1,44 @@
 <?php
-// exercicío 2
-
-    function areaTriangulo($base, $altura){
-        $area = $base * ($altura/2);
-        echo $area;
+if (isset($_POST["base"]) && isset($_POST["altura"])) {
+    $base = $_POST["base"];
+    $altura = $_POST["altura"];
+    function Calcular($base, $altura)
+    {
+        return ($base * $altura)/2;
     }
 
-    areaTriangulo(1,4)
+    echo "<h2>A área do triangulo é: " . Calcular($base, $altura) . "</h2>";
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área do Triangulo</title>
+</head>
+
+<body>
+
+    <form action="" method="POST">
+        <div>
+            <label for="base">Base</label>
+            <input type="number" name="base" id="base">
+        </div>
+
+        <div>
+            <label for="altura">Área</label>
+            <input type="number" name="altura" id="altura">
+        </div>
+
+        <button type="submit">
+            Calcular
+        </button>
+    </form>
+
+</body>
+
+</html>
